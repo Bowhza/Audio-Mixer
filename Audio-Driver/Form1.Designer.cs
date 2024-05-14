918de2af952902b625d0927aa68c61c9c26525cf
Bowhza <bogtank2003@gmail.com>
2024-05-13 22:50:13 -0600

    Update form start position and size.

--- Audio-Driver/Form1.Designer.cs
+++ Audio-Driver/Form1.Designer.cs
@@ -242,7 +242,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Sessions_List);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(750, 450);
+            this.MinimumSize = new System.Drawing.Size(750, 450);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Audio Driver";
             this.ResumeLayout(false);
             this.PerformLayout();
